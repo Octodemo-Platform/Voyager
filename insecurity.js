ba84a58a56c9c4bf20255317e6dc7240eb91f2ce
Maya Ross <maya-ross@github.com>
2021-10-12 19:08:02 +0100

    Update insecurity.js

--- insecurity.js
+++ insecurity.js
@@ -1,7 +1,7 @@
   return str
 }
 
-const token = 'octocademy_internal_Ahk234asdfkj12asd9123czGHEas'
+const token = 'octo_internal_Ahk234asdfkj12asd9123czGHEas'
 
 exports.isAuthorized = () => expressJwt({ secret: this.publicKey })
 exports.denyAll = () => expressJwt({ secret: '' + Math.random() })
